89ab47cda7aff3583be2154e1963111bf4b4ff56
chadcarlson <chad.carlson@platform.sh>
2020-03-18 18:08:12 -0400

    Clean up. Try to set.

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
 MM_SERVICESETTINGS_SITEURL=$(echo "$PLATFORM_ROUTES" | base64 --decode | jq -r 'to_entries[] | select(.value.primary) | .key') ./bin/mattermost
-
-# MM_SERVICESETTINGS_SITEURL=$(echo "$PLATFORM_RELATIONSHIPS" | base64 --decode | jq -r )
-#
-# ( echo "$PLATFORM_RELATIONSHIPS" | base64 --decode | jq .database )
