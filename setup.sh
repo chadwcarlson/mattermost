90a40eeac10941b557fadfc4c13395eae2647a0f
chadcarlson <chad.carlson@platform.sh>
2020-03-18 12:53:01 -0400

    handle files.

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ MATTERMOST_VERSION=5.21.0
 wget https://releases.mattermost.com/${MATTERMOST_VERSION}/mattermost-${MATTERMOST_VERSION}-linux-amd64.tar.gz;
 
 tar xzf mattermost-${MATTERMOST_VERSION}-linux-amd64.tar.gz;
+rm mattermost-${MATTERMOST_VERSION}-linux-amd64.tar.gz
+cp -Rf mattermost/* ../
