503ce5087505db9169b40cc50730956c61b17e44
chadcarlson <chad.carlson@platform.sh>
2020-03-18 15:43:49 -0400

    set config dynamically from env with jq.

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 MM_SERVICESETTINGS_SITEURL=$(echo "$PLATFORM_ROUTES" | base64 --decode | jq -r 'to_entries[] | select(.value.primary) | .key') ./bin/mattermost
+
+# MM_SERVICESETTINGS_SITEURL=$(echo "$PLATFORM_RELATIONSHIPS" | base64 --decode | jq -r )
+#
+# ( echo "$PLATFORM_RELATIONSHIPS" | base64 --decode | jq .database )
