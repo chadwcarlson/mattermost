305a66a25be7cbec1cbdfe675ba01c7284edae3a
chadcarlson <chad.carlson@platform.sh>
2020-03-18 12:26:03 -0400

    Syntax.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-$MATTERMOST_VERSION=5.21.0
+MATTERMOST_VERSION=5.21.0
 
 wget https://releases.mattermost.com/${MATTERMOST_VERSION}/mattermost-${MATTERMOST_VERSION}-linux-amd64.tar.gz;
 
